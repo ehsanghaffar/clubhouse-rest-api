f4a23fa354cdcdf2b94e87152c105d3019efbc18
ehsanghaffarii <ghafari.5000@gmail.com>
2022-09-19 06:33:44 +0430

    initial structure and add helpers

---
+++ src/server.js
@@ -0,0 +1,8 @@
+// Server Dependencies
+const express = require("express");
+const http = require("http");
+const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
+const cors = require("cors");
+const morgan = require("morgan");
+const helmet = require("helmet");
